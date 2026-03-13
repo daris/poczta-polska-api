aea2c6d675087dbe7fee02da78c4dfab16e2f7b1
daris <daris91@gmail.com>
2026-03-13 10:33:22 +0100

    initial commit

---
+++ src/test/kotlin/com/example/pocztapolskaapi/PocztaPolskaApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.pocztapolskaapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PocztaPolskaApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
